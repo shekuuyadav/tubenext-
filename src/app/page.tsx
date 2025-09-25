bc7cd31e24ea3a03d82b46bf8189e317781a8519
shekuuyadav <singhabhiyadav420@gmail.com>
2025-09-25 21:17:01 +0530

    fix: wrap Header + HomePageWrapper in Suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-
 import { HomePageWrapper } from '@/components/HomePageWrapper';
 import { Suspense } from 'react';
 import { Header } from '@/components/Header';
@@ -7,14 +6,14 @@ import { VideoGridSkeleton } from '@/components/HomePage';
 export default function Home() {
   return (
     <div className="flex min-h-screen w-full flex-col">
-      <Header />
-      <main className="flex-1 p-4 md:p-6 lg:p-8">
-        <div className="mx-auto w-full max-w-screen-2xl">
-          <Suspense fallback={<VideoGridSkeleton />}>
+      <Suspense fallback={<VideoGridSkeleton />}>
+        <Header />
+        <main className="flex-1 p-4 md:p-6 lg:p-8">
+          <div className="mx-auto w-full max-w-screen-2xl">
             <HomePageWrapper />
-          </Suspense>
-        </div>
-      </main>
+          </div>
+        </main>
+      </Suspense>
     </div>
   );
 }
